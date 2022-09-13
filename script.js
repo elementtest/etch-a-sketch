42b4f1402f0e1e82febf21f10a6a0fc901a81cc5
elementtest <94732196+elementtest@users.noreply.github.com>
2022-09-13 11:35:01 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var div1 = document.createElement("div");
-div1.classList.add('div1');
-console.log(div1);
-var gridContainer = document.querySelector('#grid-container');
-
-gridContainer.appendChild(div1);
