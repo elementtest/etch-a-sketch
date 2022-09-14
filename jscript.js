45ab179859eb06a445c8f4d4808a2a5eee69469c
elementtest <elementtest7@gmail.com>
2022-09-14 12:51:22 +0800

    first successful grid box!

---
+++ jscript.js
@@ -0,0 +1,6 @@
+for(x=0; x<256;x++) {
+	var divVe = document.createElement('div');
+	document.querySelector('.container-grid').appendChild(divVe);
+	divVe.classList.add('div-grid');
+	divVe.style.backgroundColor = 'blue';
+}
