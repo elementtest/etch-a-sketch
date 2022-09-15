74b94840d80da193ae0bd69d7ea88f8f07a7d241
elementtest <elementtest7@gmail.com>
2022-09-15 11:05:02 +0800

    change css add button

--- jscript.js
+++ jscript.js
@@ -1,6 +1,12 @@
 for(x=0; x<256;x++) {
 	var divVe = document.createElement('div');
 	document.querySelector('.container-grid').appendChild(divVe);
-	divVe.classList.add('div-grid');
+	divVe.classList.add('divGrid');
 	divVe.style.backgroundColor = 'blue';
+	divVe.addEventListener("mouseenter", function(DtripD) {
+		DtripD.target.style.backgroundColor = "purple";
+	});
+	divVe.addEventListener("mouseleave", function(DtripD){
+		DtripD.target.style.backgroundColor = "blue";
+	})
 }
