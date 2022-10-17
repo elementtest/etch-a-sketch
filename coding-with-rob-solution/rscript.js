6170ddf196cef96aa8fa6bac221c9ad071098f72
elementtest <elementtest7@gmail.com>
2022-10-17 16:21:20 +0800

    add complete verbatum solution to etch a sketch

--- coding-with-rob-solution/rscript.js
+++ coding-with-rob-solution/rscript.js
@@ -1,17 +1,36 @@
-function populateBoard(size) {
-	let board = document.querySelector(".board");
-	board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-	board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-	//MISTAKE used single quotes and not backtickes
-	//needed backticks
-	//MISTAKE repeat cannot have a space have to use repeat and immediately put 
-	//template literals are indicated by the backtick character
-	//you can use a placeholder with the ${} with a variable name inside
-	for (let i = 0; i < 256; i++) {
-  		let square = document.createElement("div");
-  		square.style.backgroundColor = "blue";
-  		board.insertAdjacentElement("beforeend", square);
-	};
-};
+// function populateBoard(size) {
+// 	let board = document.querySelector(".board");
+// 	let squares = board.quersySelectorAll("div");
+// 	squares.forEach((div) => div.remove());
+// 	board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+// 	board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-populateBoard(16);
+// 	let amount = size * size;
+// 	for (let i = 0; i < amount; i++) {
+//   		let square = document.createElement("div");
+//   		square.style.backgroundColor = "blue";
+//   		board.insertAdjacentElement("beforeend", square);
+// 	}
+// }
+
+// populateBoard(16);
+
+// function changeSize(input) {
+// 	populateBoard(input);
+// }
+
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
